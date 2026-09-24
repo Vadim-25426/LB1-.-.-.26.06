1601c65dff9e0d637a08c4bf379f35ed831493d0
Вадим Даутов <aks2323@yandex.ru>
2026-09-24 22:10:03 +0300

    Внесена фунция разборщика

--- cli.py
+++ cli.py
@@ -1,27 +1,35 @@
 import argparse
 import sys
+from calc.stats import *
+
+
 argv=sys.argv[1:]
 
 def build_razb():
-    parser=argparse.ArgumentParser(prog='mathtool',allow_abbrev=False)
+    parser=argparse.ArgumentParser(prog='mathtool',allow_abbrev=False, description='crmevoe')
     subparsers=parser.add_subparsers(dest='command', help='Доступные команды')
     a = subparsers.add_parser('integrate', allow_abbrev=False)
-    a.add_argument('--steps',type=int, required=True, help='Щаги')
-    a.add_argument('--from',dest='start', required=True, help='Начало интервала')
-    a.add_argument('--to',  required=True, help='Конец интервала')
-    a.add_argument('--func', type=int, required=True, help='Функция')
-    a.add_argument('-a', type=int,  help='Number of steps')
-    a.add_argument('-b', type=int, help='Number of steps')
-    a.add_argument('-c', type=int, help='Number of steps')
-    a.add_argument('--input', type=int, help='Number of steps')
+    a.add_argument('-a', type=int, help='Аргумент а')
+    a.add_argument('-b', type=int, help='Аргумент b')
+    a.add_argument('-c', type=int, help='Аргумент с')
 
+    b = subparsers.add_parser('stats', allow_abbrev=False)
+    b.add_argument('--steps',type=int, required=True, help='Шаги')
+    b.add_argument('--from',dest='start', required=True, help='Начало интервала')
+    b.add_argument('--to',  required=True, help='Конец интервала')
+    b.add_argument('--func', required=True, help='Функция')
+    b.add_argument('--input', dest='vvod',type=str, help='')
 
+    c = subparsers.add_parser('series', allow_abbrev=False)
+    ser = c.add_mutually_exclusive_group(required=True)
+    ser.add_argument('--terms', type=int)
+    ser.add_argument('--eps', type=float)
     return parser
-if __name__=='__main__':
-    parser=build_razb()
-    args=parser.parse_args(argv)
-    if args.command is None:
-        parser.print_help()
-        sys.exit(0)
-    if args.command == 'integrate':
+
+
+
+
+
+
+
 
