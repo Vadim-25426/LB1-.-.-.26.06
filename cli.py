855625e0d3e075a3d7d5fa6cf68659a9ecd1a7ce
Вадим Даутов <aks2323@yandex.ru>
2026-09-25 20:43:48 +0300

    Полностью исправлен парсер, добавлены уточняющие описания для функций

--- cli.py
+++ cli.py
@@ -1,33 +1,34 @@
 import argparse
-import sys
-from calc.stats import *
-
-
-argv=sys.argv[1:]
 
 def build_razb():
-    parser=argparse.ArgumentParser(prog='mathtool',allow_abbrev=False, description='crmevoe')
-    subparsers=parser.add_subparsers(dest='command', help='Доступные команды')
-    a = subparsers.add_parser('integrate', allow_abbrev=False)
-    a.add_argument('-a', type=int, help='Аргумент а')
-    a.add_argument('-b', type=int, help='Аргумент b')
-    a.add_argument('-c', type=int, help='Аргумент с')
+    parser = argparse.ArgumentParser(prog='mathtool',allow_abbrev=False,description='Инструмент для математических вычислений')
+    subparsers = parser.add_subparsers(dest='command', help='Доступные команды')
 
-    b = subparsers.add_parser('stats', allow_abbrev=False)
-    b.add_argument('--steps',type=int, required=True, help='Шаги')
-    b.add_argument('--from',dest='start', required=True, help='Начало интервала')
-    b.add_argument('--to',  required=True, help='Конец интервала')
-    b.add_argument('--func', required=True, help='Функция')
-    b.add_argument('--input', dest='vvod',type=str, help='')
+    #solve
+    sol_p = subparsers.add_parser('solve', allow_abbrev=False, help='Решение уравнения')
+    sol_p.add_argument('-a', type=int, help='Коэффициент A')
+    sol_p.add_argument('-b', type=int, help='Коэффициент B')
+    sol_p.add_argument('-c', type=int, help='Коэффициент C')
 
-    c = subparsers.add_parser('series', allow_abbrev=False)
-    ser = c.add_mutually_exclusive_group(required=True)
-    ser.add_argument('--terms', type=int)
-    ser.add_argument('--eps', type=float)
+    #stats
+    stats_p = subparsers.add_parser('stats', allow_abbrev=False, help='Показатели последовательности')
+    stats_p.add_argument('--input', type=str, help='Файл с числами')
+
+    #series
+    ser = subparsers.add_parser('series', allow_abbrev=False, help='Сумма ряда')
+    ser.add_argument('--func', required=True, help='Ряд: third, sqplus')
+    gr = ser.add_mutually_exclusive_group(required=True)
+    gr.add_argument('--terms', type=int, help='Количество слагаемых')
+    gr.add_argument('--eps', type=float, help='Точность')
+
+    #integrate
+    int_p = subparsers.add_parser('integrate', allow_abbrev=False, help='Численное интегрирование')
+    int_p.add_argument('--func', required=True, help='Функция: ratio, root')
+    int_p.add_argument('--from', dest='start', type=float, required=True, help='Нижний предел')
+    int_p.add_argument('--to', type=float, required=True, help='Верхний предел')
+    int_p.add_argument('--steps', type=int, required=True, help='Количество шагов')
+
     return parser
-
-
-
 
 
 
