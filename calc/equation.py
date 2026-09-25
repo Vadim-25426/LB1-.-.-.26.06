ebd98b5a0da95d235399a833b83379ee39eb1362
Вадим Даутов <aks2323@yandex.ru>
2026-09-25 21:21:04 +0300

    Сделал чтобы фунция solve вщзвращала кортеж + доделал проверку коэффицентов

--- calc/equation.py
+++ calc/equation.py
@@ -1,30 +1,32 @@
 import math
 
-def solve(a,b,c):
-    if a<=10000 and b<=10000 and c<=10000:
-        try:
-            if a == 0:
-                # Линейное
-                if b != 0:
-                    return f'Уравнение линейное: x={-c / b:.3f}'
-                # Не является уравнением
-                else:
-                    return 'Не уравнение: неизвестного нет; код возврата 1'
-            # Квадратное
-            else:
-                D = b ** 2 - 4 * a * c
-                if D > 0:
-                    x1 = (-b + math.sqrt(D)) / (2 * a)
-                    x2 = (-b - math.sqrt(D)) / (2 * a)
-                    return f'Квадратное; D={D}; x1={x1:.3f}\nx2={x2:.3f}'
-                elif D == 0:
-                    x = -b / (2 * a)
-                    return f'Квадратное; D={D}; x={x:.3f}'
-                else:
-                    return 'Действительных корней нет'
-        except ValueError:
-            raise ValueError('Ошибка: коэффицент не является целым числом; код возврата: 1')
+MAX_VALUE = 10000
+
+
+def check_coefficients(coefficients: dict[str, int]):
+    #Проверка коэффициентов на допустимый диапазон
+    for name, value in coefficients.items():
+        if abs(value) > MAX_VALUE:
+            raise ValueError(f"коэффициент {name} вне допустимого диапазона")
+
+
+def solve(a: int, b: int, c: int):
+
+    if a == 0:
+        if b == 0:
+            # Не уравнение
+            return "не уравнение", None, []
+        # Линейное
+        x = -c / b
+        return "линейное", None, [x]
+
+    D = b ** 2 - 4 * a * c
+    if D > 0:
+        x1 = (-b + math.sqrt(D)) / (2 * a)
+        x2 = (-b - math.sqrt(D)) / (2 * a)
+        return "квадратное", D, [x1, x2]
+    elif D == 0:
+        x = -b / (2 * a)
+        return "квадратное", D, [x]
     else:
-        return 'Число по модулю превышает 10000'
-d=solve(0, 1000 ,3)
-print(d)
+        return "квадратное", D, []
